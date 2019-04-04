6ca611463f3e6a93adc5db9df8ada31c904572f0
Турлюн Евгений Федорович <TEF@e-tomsk.loc>
2019-04-04 15:12:06 +0700

    Logger added

--- src/main/java/ru/turlyunef/App.java
+++ src/main/java/ru/turlyunef/App.java
@@ -1,13 +1,15 @@
 package ru.turlyunef;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        log.info("Start the program");
     }
 }
